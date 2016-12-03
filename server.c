e43b5a1db7d4183c94e19955de69e943055fa4eb
Nyloner <nyloner.root@gmail.com>
2016-12-03 21:34:28 +0800

    create httpsserver

---
+++ server.c
@@ -0,0 +1,142 @@
+#include "server.h"
+
+
+int init()
+{
+    struct sockaddr_in name;
+    struct sockaddr_in remote_client;
+    unsigned int client_len;
+
+    int server_socket=socket(PF_INET, SOCK_STREAM, 0);
+    if(server_socket==-1)
+    {
+        printf("Error\n");
+        exit(-1);
+    }
+
+    //绑定IP和端口
+    name.sin_family = AF_INET;
+    name.sin_port = htons(SERVER_PORT);
+    name.sin_addr.s_addr = htonl(INADDR_ANY);
+    if(bind(server_socket,(struct sockaddr *)&name, sizeof(name))==-1)
+    {
+        printf("Bind Error !\n");
+        exit(-1);
+    }
+    //开始监听
+    if(listen(server_socket, 5)==-1)
+    {
+        printf("Listen Error !\n");
+        exit(-1);
+    }
+
+    while(1)
+    {
+        int client_sock=accept(server_socket,(struct sockaddr *)&remote_client,&client_len);
+        if(client_sock!=-1)
+        {
+            pthread_t client;
+            pthread_create(&client,NULL,(void *)accept_request,(void *)(intptr_t)client_sock);
+        }
+    }
+    return 0;
+}
+
+int get_line(int client_sock,char *buf,int size)
+{
+    char c='0';
+    int read_size=0;
+    while(read_size<size-1&&c!='\n')
+    {
+        int num=recv(client_sock,&c,1,0);
+        if(num>0)
+        {
+            if(c=='\r')
+            {
+                num=recv(client_sock,&c,1,MSG_PEEK);
+                if(num>0&&c=='\n')
+                    recv(client_sock,&c,1,0);
+                else
+                    c='\n';
+            }
+            buf[read_size]=c;
+            read_size++;
+        }
+        else
+        {
+            c='\n';
+        }
+    }
+    buf[read_size] = '\0';
+    return read_size;
+}
+
+void accept_request(void *arg)
+{
+    int client=(intptr_t)arg;
+    char buf[1024];
+    char method[255];
+    char url[255];
+    int read_size;
+
+    read_size=get_line(client,buf, sizeof(buf));
+
+    int index=0;
+    while(buf[index]!=' '&&index<read_size)
+    {
+        method[index]=buf[index];
+        index++;
+    }
+    method[index]='\0';
+
+    index++;
+    int url_i=0;
+    while(buf[index]!=' '&&index<read_size&&url_i<sizeof(url))
+    {
+        url[url_i]=buf[index];
+        url_i++;
+        index++;
+    }
+    url[url_i]='\0';
+    if(!strcmp(method,"GET"))
+    {
+
+    }
+    if(!strcmp(method,"POST"))
+    {
+
+    }
+    not_found(client);
+    close(client);
+}
+
+void not_found(int client)
+{
+    char buf[1024];
+    sprintf(buf, "HTTP/1.1 404 NOT FOUND\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Server: NyServer/0.1.0\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: text/html;charset=utf-8\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
+    FILE *fp;
+    if((fp=fopen("www/404.html","r"))==NULL)
+    {
+        sprintf(buf, "<HTML><TITLE>Not Found</TITLE>\r\n");
+        send(client, buf, strlen(buf), 0);
+        sprintf(buf, "<BODY><P>404 Not Found</P>\r\n");
+        send(client, buf, strlen(buf), 0);
+        sprintf(buf, "</BODY></HTML>\r\n");
+        send(client, buf, strlen(buf), 0);
+        return;
+    }
+    while(fgets(buf,1024,fp)!=NULL)
+    {
+        send(client,buf,strlen(buf),0);
+        sprintf(buf, "\r\n");
+        send(client, buf, strlen(buf),0);
+    }
+    fclose(fp);
+}
