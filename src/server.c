86278be19f495e1e4c5736d98a193fbd4e36954d
Nyloner <nyloner.root@gmail.com>
2017-03-17 11:30:39 +0800

    update

--- src/server.c
+++ src/server.c
@@ -29,7 +29,7 @@ void init_server()
         exit(-1);
     }
 
-    pool=init_thread_pool(30);
+    pool=init_thread_pool(20);
     pthread_create(&keepalive_pid,NULL,(void *)thread_pool_keepalive,(void *)pool);
     //开始监听
     if(listen(server_socket, 5)==-1)
