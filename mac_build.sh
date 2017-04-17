f7662480b50885032f59054e456c373675bf4236
Nyloner <nyloner.root@gmail.com>
2017-04-17 18:58:36 +0800

    delete mac

--- mac_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-gcc -c mac_src/main.c -o main.o
-gcc -pthread -c mac_src/threadpool.c -o threadpool.o
-gcc -pthread -c mac_src/server.c -o server.o
-gcc -pthread -c mac_src/views.c -o views.o
-gcc -pthread -o server main.o threadpool.o server.o views.o
-rm main.o threadpool.o server.o views.o
